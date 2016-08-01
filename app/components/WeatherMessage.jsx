c73d726a2523d397c28d39a6b9599024949859a6
cyborg <kevinthecyborg@Kevins-MacBook-Pro.local>
2016-08-01 09:12:53 -0700

    initial commit

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+// var WeatherMessage = React.createClass({
+//   render: function () {
+//     var {temp, location} = this.props;
+//
+//     return (
+//       <p>It's {temp} in {location}.</p>
+//     );
+//   }
+// });
+
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <p>It's {temp} in {location}.</p>
+  );
+}
+
+module.exports = WeatherMessage;
