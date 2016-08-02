f9813d9bdf7d94eae9682578176a068fb2645f02
cyborg <kevinthecyborg@Kevins-MacBook-Pro.local>
2016-08-02 10:29:57 -0700

    switched to scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ var Examples = require('Examples');
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 // Custom CSS
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
